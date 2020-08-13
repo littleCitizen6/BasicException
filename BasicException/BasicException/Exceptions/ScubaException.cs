967f84c48eca6ecbef926c00a1123336b049fb1c
littleCitizen6 <thelittlecitizen6@gmail.com>
2020-08-13 10:07:40 +0300

    created scubaException

---
+++ BasicException/BasicException/Exceptions/ScubaException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicException.Exceptions
+{
+    public class ScubaException : Exception
+    {
+        private string _userName;
+        public override string Message => $"hey {_userName}, you have a scuba exception please check your scuba ";
+        public ScubaException()
+        {
+            _userName = Environment.UserName;
+        }
+
+        
+    }
+}
